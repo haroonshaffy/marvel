fc146c9c8e687f4b160876d80dc6922b608d9134
Haroon Shaffiulla <haroonshaffy@gmail.com>
2021-08-22 08:13:00 -0400

    housekeeping

--- comic.php
+++ comic.php
@@ -16,11 +16,9 @@
         <a href="./index.php">Home</a>
         <div class="d-flex align-items-center container" id="comicsSpinnerSection"></div>
         <div class="container single-comic__container" id="singleComicContainerDiv">
-        </div> <!-- /container -->
-    </div> <!-- /jumbotron -->
+        </div>
+    </div>
     <script src="main.js"></script>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
--- creator.php
+++ creator.php
@@ -20,11 +20,9 @@
         <div class="d-flex align-items-center container" id="comicCreatorSpinnerSection"></div>
         <div class="d-flex align-items-center container" id="comicCreatorSpinnerSection1"></div>
         <div class="container comic-creator__container" id="comicCreatorContainerDiv">
-        </div> <!-- /container -->
-    </div> <!-- /jumbotron -->
+        </div>
+    </div>
     <script src="main.js"></script>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
